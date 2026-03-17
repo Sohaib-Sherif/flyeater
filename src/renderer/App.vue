df48c6af80d9b0b24963f0c50f57de5f9c6ed8e6
Sohaib Sherif <triples17t@gmail.com>
2026-03-17 03:43:54 +0200

    refactor(renderer): Structure vue app for development

---
+++ src/renderer/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from './base/components/ui/button';
+
+  console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
+
+<template>
+  <Button>Hey</Button>
+  <h1>💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+</template>
