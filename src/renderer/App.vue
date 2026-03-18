684fb3ae68fd01cdc28d8f88bafa17b3266f67fa
Sohaib Sherif <triples17t@gmail.com>
2026-03-18 07:14:02 +0200

    feat(renderer): show machines in the tray window

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -2,6 +2,8 @@
 import { onMounted, ref } from 'vue';
 import { Button } from './base/components/ui/button';
 import { Machine } from '../../flysdk/api';
+import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from './base/components/ui/item';
+import { PlayIcon, SquareIcon } from 'lucide-vue-next';
 
 const machines = ref<Machine[]>([])
 
@@ -11,5 +13,23 @@ onMounted(async () => {
 </script>
 
 <template>
-  <p v-for="(machine, index) in machines" :key="index">{{ machine.name }}</p>
+  <Item 
+    v-for="(machine, index) in machines" 
+    :key="index"
+    variant="outline"
+  >
+    <ItemContent>
+      <ItemTitle>{{ machine.name }}</ItemTitle>
+      <ItemDescription>
+        {{ machine.region }} - {{ machine.updated_at }}
+      </ItemDescription>
+    </ItemContent>
+    <ItemActions>
+      <Button :variant="machine.state === 'started' ? 'destructive': 'default'" size="sm">
+        <SquareIcon v-if="machine.state ==='started'"></SquareIcon>
+        <PlayIcon v-else></PlayIcon>
+        {{ machine.state === 'started'? 'Stop' : 'Start' }}
+      </Button>
+    </ItemActions>
+  </Item>
 </template>
