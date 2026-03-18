f8e8194c9207e7c799a6e682bef748ef7ee25ac7
Sohaib Sherif <triples17t@gmail.com>
2026-03-18 04:42:43 +0200

    feat(preload): Add bridge for ipc handler of list machines api request

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld('flyApi', {
+    listMachines: () => ipcRenderer.invoke('machines:list')
+})
