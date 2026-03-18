359e95f5c9387c8c34c9dc437b02968443aa4834
Sohaib Sherif <triples17t@gmail.com>
2026-03-18 04:44:10 +0200

    refactor(renderer): Call ipc handler to list machines in fly.io

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import { Button } from './base/components/ui/button';
+import { Machine } from '../../flysdk/api';
 
-  console.log('👋 This message is being logged by "App.vue", included via Vite');
+const machines = ref<Machine[]>([])
+
+onMounted(async () => {
+  machines.value = await window.flyApi.listMachines();
+})
 </script>
 
 <template>
-  <Button>Hey</Button>
-  <h1>💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <p v-for="(machine, index) in machines" :key="index">{{ machine.name }}</p>
 </template>
