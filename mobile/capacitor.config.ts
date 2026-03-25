1ffd75cd6050ba360981a4319e8925ac0d83892b
Sohaib Sherif <triples17t@gmail.com>
2026-03-26 01:39:06 +0200

    feat(mobile): enable Http plugin in capacitor config

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'patiently.sits.flyeater',
   appName: 'flyeater',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
